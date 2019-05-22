1543b31f3a17880c774bc58a0282e0944e698c3d
VBTX <jv.svedaite@gmail.com>
2019-05-22 08:07:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper'
+
+
+
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+
+
+
+
+
+
+
+
+let shortGreeting = "Welcome, " + currentUser.slice(0, 1) + '!';
